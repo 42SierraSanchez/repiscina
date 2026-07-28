a8d988ad1f8947b92686ca8d01e8aaa78bc8dc5a
Angel Sierra Sanchez <asierra@c1r3s6.42malaga.com>
2026-07-28 20:51:57 +0200

    holamundo

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: asierra <asierra@student.42malaga.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 19:46:07 by asierra           #+#    #+#             */
-/*   Updated: 2026/07/28 16:50:06 by asierra          ###   ########.fr       */
+/*   Updated: 2026/07/28 17:16:23 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,30 @@ unsigned int	ft_strlen(char *str)
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int				i;
+	int	i;
 
 	i = 0;
-	while (src[i] && dest[i] && (unsigned int)i < size - 1)
+	if (size > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && (unsigned int)i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
 	}
-	dest[size] = '\0';
+	dest[size - 1] = '\0';
 	return (ft_strlen(src));
 }
 
 int	main(int argc, char **argv)
 {
 	unsigned int	size;
+	char			dest[size];
 
 	if (argc != 2)
 		return (printf("You must use only one argument\n"));
-	
 	size = ft_strlen(argv[1]) + 1;
-	char dest[size];
 	ft_strlcpy(dest, argv[1], size);
-
 	printf("src is : %s\n", argv[1]);
 	printf("dest es : %s\n", dest);
 	return (0);
