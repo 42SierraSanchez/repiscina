76939b6b768ea08fc3ca1a95e9a7db6f272a9a75
Angel Sierra Sanchez <asierra@c1r6s6.42malaga.com>
2026-07-29 13:55:10 +0200

    holamundo

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,29 +6,51 @@
 /*   By: asierra <asierra@student.42malaga.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 23:08:39 by asierra           #+#    #+#             */
-/*   Updated: 2026/07/28 23:17:19 by asierra          ###   ########.fr       */
+/*   Updated: 2026/07/29 13:53:46 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
-int	ft_str_is_printable(char *str)
+void	ft_putchar(char c)
 {
-	if (!str[0])
-		return (1);
-	if (!(str[0] >= 32 && str[0] <= 127))
-		return (0);
-	return (ft_str_is_printable(str + 1));
+	write(1, &c, 1);
 }
 
-int	print_non_print(char *str)
+int	char_non_print(char c)
 {
-	if
-	return(0)
+	if (c >= 32 && c <= 126)
+		return (0);
+	return(1);
+}
+
+void	print_non_print(char *str, char *base)
+{
+	if (char_non_print(str[0]))
+	{
+		ft_putchar('\\');
+		ft_putchar(base[str[0] / 16]);
+		ft_putchar(base[str[0] % 16]);
+	}	
+	else 
+		ft_putchar(str[0]);
+	if (str[0])
+		print_non_print(&str[1], base);
 }
 
 void	ft_putstr_non_printable(char *str)
 {
-	print_non_print(str);
+	print_non_print(str, "0123456789abcdef");
+}
+
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (printf("You must use only one argument\n"));
+	printf("original is : %s\n\n non print is: \n", argv[1]);
+	ft_putstr_non_printable(argv[1]);
+	printf("\n");
+	return (0);
 }
