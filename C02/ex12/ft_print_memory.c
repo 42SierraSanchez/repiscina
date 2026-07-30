209af0477df121df9415ebfe7337b48cca829bf0
Angel Sierra Sanchez <asierra@c1r7s3.42malaga.com>
2026-07-30 13:13:46 +0200

    mumalmumalmumal

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -6,7 +6,7 @@
 /*   By: asierra <asierra@student.42malaga.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 15:40:30 by asierra           #+#    #+#             */
-/*   Updated: 2026/07/30 12:58:41 by asierra          ###   ########.fr       */
+/*   Updated: 2026/07/30 13:13:10 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,20 @@ int	is_printable(char c)
 	return (c >= 32 && c <= 126);
 }
 
-void	ft_printmem(void *addr, unsigned int count, unsigned int size)
+void	ft_printmem(unsigned char *byte_str, unsigned int count, unsigned int size)
 {
-	if (!addr || count >= size)
+	if (!byte_str || count >= size)
 		return ;
-	else
-		ft_putchar(*(char *)addr);
-	ft_printmem(addr + 1, count + 1, size);
+	ft_putchar(*byte_str);
+	ft_printmem(byte_str + 1, count + 1, size);
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	ft_printmem(addr, 0, size);
+	unsigned char *byte_str;
+
+	byte_str = (unsigned char *)addr;
+	ft_printmem(byte_str, 0, size);
 	return (addr);
 }
 
@@ -43,5 +45,5 @@ int	main(void)
 	char	*test;
 
 	test = "0123456789abcdef";
-	ft_print_memory(&test, 16);
+	ft_print_memory(test, 16);
 }
