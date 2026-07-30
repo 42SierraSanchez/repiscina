ccf808c80d064c3766e41a7bc9ae3bbfaca83643
Angel Sierra Sanchez <asierra@c1r7s3.42malaga.com>
2026-07-30 13:06:58 +0200

    mumalmumalmumal

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -6,13 +6,12 @@
 /*   By: asierra <asierra@student.42malaga.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 15:40:30 by asierra           #+#    #+#             */
-/*   Updated: 2026/07/29 18:32:12 by asierra          ###   ########.fr       */
+/*   Updated: 2026/07/30 12:58:41 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
@@ -24,33 +23,25 @@ int	is_printable(char c)
 	return (c >= 32 && c <= 126);
 }
 
-void	ft_printmem(void *addr, unsigned int count)
+void	ft_printmem(void *addr, unsigned int count, unsigned int size)
 {
-	if (!addr || count >= 16)
+	if (!addr || count >= size)
 		return ;
 	else
 		ft_putchar(*(char *)addr);
-	ft_printmem(addr + 1, count + 1);
+	ft_printmem(addr + 1, count + 1, size);
 }
 
-void *ft_print_memory(void *addr, unsigned int size)
+void	*ft_print_memory(void *addr, unsigned int size)
 {
-	ft_printmem(addr, size);
-	printf("printf: puntero es %016lx", (uintptr_t)addr);
-	return(addr);
+	ft_printmem(addr, 0, size);
+	return (addr);
 }
-
 
 int	main(void)
 {
-	char *test;
-	uintptr_t **p;
+	char	*test;
 
 	test = "0123456789abcdef";
-	p = &test;
-	
-	printf("%016" PRIxPTR "\n", p);
-
-	ft_print_memory(p, 16);
-
+	ft_print_memory(&test, 16);
 }
