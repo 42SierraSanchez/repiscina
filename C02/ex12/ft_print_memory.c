04f3f0c0e5eed7a2f586a8823b5493d511f56608
Angel Sierra Sanchez <asierra@c1r7s3.42malaga.com>
2026-07-30 15:29:08 +0200

    mumalmumalmumal

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -6,7 +6,7 @@
 /*   By: asierra <asierra@student.42malaga.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 15:40:30 by asierra           #+#    #+#             */
-/*   Updated: 2026/07/30 15:22:06 by asierra          ###   ########.fr       */
+/*   Updated: 2026/07/30 15:28:46 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@ void	ft_printmem(unsigned char *byte_str, unsigned int count,
 	hex_base = "0123456789abcdef";
 	if (count >= 16)
 		return ;
-	if (byte_str[0] && count < size)
+	if (count < size)
 	{
 		ft_putchar(hex_base[*byte_str / 16]);
 		ft_putchar(hex_base[*byte_str % 16]);
 	}
 	else
 		write(1, "  ", 2);
-	if (count > 0 && count < 16 && (count % 2))
+	if (count > 0 && count < 14 && (count % 2))
 		ft_putchar(' ');
 	ft_printmem(byte_str + 1, count + 1, size);
 }
