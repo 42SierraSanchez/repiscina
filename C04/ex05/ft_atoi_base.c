d5d1a2c99686f6f3275e716edfbdaaf510ef31ce
asierra <asierra@student.42malaga.com>
2026-08-02 23:22:20 +0200

    adsad

---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/02 22:38:07 by asierra           #+#    #+#             */
+/*   Updated: 2026/08/02 23:22:14 by asierra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+static int	ft_isspace(int c)
+{
+	if (c == ' ' || (c >= '\t' && c <= '\r'))
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	is_valid_base(char *base)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		if ((base[i] >= 9 && base[i] <= 13) || base[i] == ' ')
+			return (0);
+		i++;
+	}
+	i = 0;
+	j = 0;
+	while (base[i])
+	{
+		while (base[j])
+		{
+			if (base[i] == base[j] && i != j)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+int	convert(char *str, char *base)
+{
+	int	result;
+	int	i;
+	int	j;
+	int	base_size;
+
+	base_size = ft_strlen(base);
+	i = 0;
+	result = 0;
+	while (str[i])
+	{
+		while (base[j])
+		{
+			j = 0;
+			if (!base[j])
+				return (result);
+			if (str[i] == base[j])
+				result = (result * base_size) + j;
+			j++;
+		}
+		i++;
+	}
+	return (result);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	neg;
+	int	result;
+
+	i = 0;
+	neg = 1;
+	result = 0;
+	if (is_valid_base(base))
+		return (0);
+	while (ft_isspace(str[i]))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			neg *= -1;
+		}
+		i++;
+	}
+	result = convert(str, base);
+	return (result * neg);
+}
+
+int	main(void)
+{
+	printf("result = %d\n", ft_atoi_base("12", "1234567890"));
+	return (0);
+}
