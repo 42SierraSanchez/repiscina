826e7eccb666c485404a253862c28ea616920e25
asierra <asierra@student.42malaga.com>
2026-08-02 19:02:02 +0200

    554

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asierra <asierra@student.42malaga.com      +#+  +:+       +#+        */
+/*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 14:05:14 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/02 16:22:53 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/02 19:01:44 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ char	*ft_strcat(char *dest, char *src)
 	j = 0;
 	while (dest[i])
 		i++;
-	
 	while (src[i])
 	{
 		dest[i] = src[j];
