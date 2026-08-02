2e37145a92900a6912f925c783ace66befd51b58
asierra <asierra@student.42malaga.com>
2026-08-02 16:17:40 +0200

    holamundo

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -27,7 +27,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 
 	dest_len = ft_strlen(dest);
 	i = 0;
-	while (i < n && src[i] != '\0')
+	while (i < nb && src[i] != '\0')
 	{
 		dest[dest_len + i] = src[i];
 		i++;
--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,54 +6,38 @@
 /*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 14:21:59 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/02 15:21:03 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/02 16:16:50 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 char	*ft_strstr(char *str, char *to_find)
 {
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	to_find_len;
 
 	i = 0;
-	j = 0;
 	if (!to_find[0])
-		return(0);
-	to_find_len = ft_strlen(to_find);
+		return (str);
 	while (str[i])
 	{
-		if (str[i] == to_find[i])
+		j = 0;
+		while (str[i + j] && str[i + j] == to_find[j])
 		{
-			while (str[i + j] == to_find[i + j])
-			{
-				if (!to_find[i + j])
-					return (&str[i]);
-				j++;
-			}
-			j = 0;
+			if (!to_find[j + 1])
+				return (&str[i]);
+			j++;
 		}
 		i++;
 	}
 	return ((void *)0);
 }
-
+/*
 int	main(void)
 {
 	char	str[] = "perr pe perros salvajes";
 	char	to_find[] = "perros";
 
 	printf("to find en str es %s", ft_strstr(str, to_find));
-}
+}*/
