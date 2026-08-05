f7b5dad163bc315bdc372e1cc3d4741d57fa4f99
asierra <asierra@student.42malaga.com>
2026-08-05 17:44:20 +0200

    SADSA

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -6,39 +6,37 @@
 /*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 14:03:36 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/05 14:25:26 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/05 17:12:13 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 ft_print_arr(int arr[10])
-	{
-		int	i;
-		
-		i = 0;
-		while (i < 10)
-		{
-			write(1, &arr[i], 1);
-		} 
-	}
+{
+	int	i;
 
-int check_col(int arr[10])
-{
-		int	i;
-		
-		i = 0;
-		while (i < 9)
-		{
-			if (arr[i] == arr[i + 1])
-				return(0);
-		} 
-		return(1);
+	i = 0;
+	while (i < 10)
+	{
+		write(1, &arr[i] + '0', 1);
+		i++;
+	}
 }
-	
-int ft_ten_queens_puzzle(void)
+
+solve(int arr[10], row)
 {
-	int arr[10];
-		
+
 	
+}
+
+int	ft_ten_queens_puzzle(void)
+{
+	int	arr[10];
+}
+
+int	main(void)
+{
+	ft_ten_queens_puzzle();
+	return (0);
 }
