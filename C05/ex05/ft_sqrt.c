6ad3ef62f0faac9ea9311a7d70e5690ef9554c83
asierra <asierra@student.42malaga.com>
2026-08-05 12:02:54 +0200

    arreglado

---
+++ C05/ex05/ft_sqrt.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/05 11:47:38 by asierra           #+#    #+#             */
+/*   Updated: 2026/08/05 12:02:46 by asierra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_sqrt(int nb)
+{
+	int candidate;
+    long candidate_sqr;
+	
+    candidate = 0;
+    
+	while (candidate <= nb)
+	{
+        candidate_sqr = (long)candidate * (long)candidate;
+		if (candidate_sqr == nb)
+			return (candidate);
+		if (candidate_sqr > nb)
+			return (0);
+		candidate++;
+	}
+	return (0);
+}
