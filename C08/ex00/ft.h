ed56b840f69efae135f9889e7a1a2ab4ff58c3b8
asierra <asierra@student.42malaga.com>
2026-08-05 18:00:49 +0200

    ng

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/05 17:45:38 by asierra           #+#    #+#             */
+/*   Updated: 2026/08/05 17:48:52 by asierra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef
+# define FT_H
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/05 17:49:28 by asierra           #+#    #+#             */
+/*   Updated: 2026/08/05 18:00:30 by asierra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE.
+}	t_bool;
+
+void	ft_putstr(char *str);
+
+#endif
