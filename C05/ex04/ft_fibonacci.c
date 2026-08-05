624d6e5f2d304d30ac7f6d7722108002ba2de225
asierra <asierra@student.42malaga.com>
2026-08-05 11:46:20 +0200

    corregido fibo

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,17 +6,17 @@
 /*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 21:03:46 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/05 11:44:23 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/05 11:46:13 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_fibonacci(int index)
+int	ft_fibonacci(int index)
 {
-    if (index < 0)
-        return(0);
-    if (index == 0)
-        return(0)
-    if (index == 1)
-        return(1);
-     return(ft_fibonacci(index - 2) + ft_fibonacci(index -1));
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
 }
