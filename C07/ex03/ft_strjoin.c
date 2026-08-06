a7b4b8106f6f43165a8e7f56582514a93402a3c7
asierra <asierra@student.42malaga.com>
2026-08-06 13:17:01 +0200

    roto

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 20:54:56 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/06 11:50:37 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/06 13:16:54 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,38 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+char	*ft_cat(char *dest, char *src)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+int	calc_total_len(int size, char **strs, char *sep)
+{
+	int	total_len;
+	int	i;
+
+	i = 0;
+	total_len = 0;
+	while (i < size)
+	{
+		total_len += ft_strlen(strs[i]);
+		i++;
+	}
+	total_len += (size - 1) * ft_strlen(sep);
+}
+
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*res;
-	int		total_len;
 	int		i;
-	int		j;
 	int		k;
-	int		l;
 
 	if (size == 0)
 	{
@@ -39,38 +63,16 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 		res[0] = '\0';
 		return (res);
 	}
-	i = 0;
-	total_len = 0;
-	while (i < size)
-	{
-		total_len += ft_strlen(strs[i]);
-		i++;
-	}
-	total_len += (size - 1) * ft_strlen(sep);
-	res = malloc((total_len + 1) * sizeof(char));
+	res = malloc((calc_total_len(size, strs, sep) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
 	i = 0;
 	k = 0;
 	while (i < size)
 	{
-		j = 0;
-		while (strs[i][j])
-		{
-			res[k] = strs[i][j];
-			j++;
-			k++;
-		}
-		l = 0;
+		res = ft_cat(&res[k], strs[i]);
 		if (i < size - 1)
-		{
-			while (sep[l])
-			{
-				res[k] = sep[l];
-				l++;
-				k++;
-			}
-		}
+			res = ft_cat(&res[k], strs[i]);
 		i++;
 	}
 	res[k] = '\0';
