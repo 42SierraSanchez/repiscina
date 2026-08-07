53dd66ea27506bcc19a1261ae2fdf64b2fbeafaf
asierra <asierra@student.42malaga.com>
2026-08-07 10:54:14 +0200

    dale cana

--- C07/ex04/ft_convert_base2.c
+++ C07/ex04/ft_convert_base2.c
@@ -6,24 +6,24 @@
 /*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 14:13:04 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/07 09:52:58 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/07 10:53:21 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_is_space(int c);
-int	ft_strlen(char *str);
-int	is_valid_base(char *base);
-int	convert(char *str, char *base);
-int	ft_atoi_base(char *str, char *base);
+int		ft_is_space(int c);
+int		ft_strlen(char *str);
+int		is_valid_base(char *base);
+int		convert(char *str, char *base);
+int		ft_atoi_base(char *str, char *base);
 
-ft_base_number_len(int nbr, char *base)
+int	ft_base_number_len(int nbr, char *base)
 {
-	int len;
-	long lnb;
-	
-	lnb = nb;
+	int		len;
+	long	lnb;
+
+	lnb = nbr;
 	len = 1;
 	if (lnb < 0)
 	{
@@ -32,58 +32,40 @@ ft_base_number_len(int nbr, char *base)
 	}
 	while (lnb /= 10)
 		len++;
-	return(len);
+	return (len);
 }
 
-
-int	ft_fill_char(int nbr, char *base, int base_size, char *conversion)
+void	ft_fill_char(int nbr, char *base, int base_size, char *conversion)
 {
-	int len;
-	
+	int	len;
+
 	len = ft_base_number_len(nbr, base);
-	conversion = malloc(len);
-		if (!conversion);
-			return(1);
 	conversion[len--] = '\0';
 	while (len)
 	{
-		conversion[len] = base[nb % base_size];
-		nb /= base_size;
+		conversion[len] = base[nbr % base_size];
+		nbr /= base_size;
 		len--;
 	}
-	return(0);
-
-
-	
-/* 	if (nb >= base_size)
-	{
-		ft_fill_char(lnb / base_size, base, base_size, conversion);
-	}
-	ft_putchar(base[lnb % base_size]); */
 }
 
-int	ft_putnbr_base(int nbr, char *base, char *conversion)
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int	base_size;
-	
-	if (!is_valid_base(base))
-		return(0);
-	base_size = ft_strlen(base);
-	ft_fill_char(nbr, base, base_size, conversion + 1);
-}
+	char	*conversion;
+	int		base_from_size;
+	int		base_to_size;
+	int		nbr_int;
+	int		len;
 
-
-char *ft_convert_base(char *nbr, char *base_from, char *base_to)
-{
-	char *conversion;
-	int base_from_size;
-	int base_to_size;
-	int num; 
-
-	base_from_size = is_valid_base(base_from)
-	base_to_size = is_valid_base(base_to)
-		if (!base_from_size || !base_to_size)
-			return(NULL);
-	ft_fill_char(num, base_to, conversion);
-return(conversion);	
+	nbr_int = ft_atoi_base(nbr, base_from);
+	len = ft_base_number_len(nbr_int, base_to);
+	conversion = malloc(len);
+	if (!conversion)
+		return ("ERROR");
+	base_from_size = is_valid_base(base_from);
+	base_to_size = is_valid_base(base_to);
+	if (!base_from_size || !base_to_size)
+		return (NULL);
+	ft_fill_char(nbr_int, base_to, base_to_size, conversion);
+	return (conversion);
 }
