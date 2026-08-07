db61c4fff899a372ac1314d11457ff4bf6f7dd34
asierra <asierra@student.42malaga.com>
2026-08-07 11:27:41 +0200

    pirata del estrecho

--- C07/ex04/ft_convert_base2.c
+++ C07/ex04/ft_convert_base2.c
@@ -6,7 +6,7 @@
 /*   By: asierra <asierra@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 14:13:04 by asierra           #+#    #+#             */
-/*   Updated: 2026/08/07 11:22:05 by asierra          ###   ########.fr       */
+/*   Updated: 2026/08/07 11:27:26 by asierra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		is_valid_base(char *base);
 int		convert(char *str, char *base);
 long	ft_atoi_base(char *str, char *base);
 
-int	ft_base_number_len(long nbr, char *base, int base_len)
+int	ft_base_number_len(long nbr, int base_len)
 {
 	int	len;
 
@@ -40,14 +40,14 @@ void	ft_fill_char(long nbr, char *base, int base_size, char *conversion)
 {
 	int	len;
 
-	len = ft_base_number_len(nbr, base, base_size);
+	len = ft_base_number_len(nbr, base_size);
 	if (nbr < 0)
 	{
 		nbr = -nbr;
 		conversion[0] = '-';
 	}
 	conversion[len--] = '\0';
-	while (len >= 0)
+	while (len >= 0 && nbr)
 	{
 		conversion[len] = base[nbr % base_size];
 		nbr /= base_size;
@@ -68,7 +68,7 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 	if (!base_from_size || !base_to_size)
 		return (NULL);
 	nbr_int = ft_atoi_base(nbr, base_from);
-	len = ft_base_number_len(nbr_int, base_to, base_to_size);
+	len = ft_base_number_len(nbr_int, base_to_size);
 	conversion = malloc(len + 1);
 	if (!conversion)
 		return (NULL);
